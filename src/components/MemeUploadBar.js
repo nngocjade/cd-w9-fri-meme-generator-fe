9f3c5a454fe7b85e61a8eeeeef0b8506365bee88
nngocjade <nngocjade@gmail.com>
2021-02-08 15:21:53 +0700

    fixed text not appearing on meme

--- src/components/MemeUploadBar.js
+++ src/components/MemeUploadBar.js
@@ -4,7 +4,7 @@ const BACKEND_API = process.env.REACT_APP_BACKEND_API;
 
 const MemeUploadBar = ({ onUpload }) => {
   const [file, setFile] = useState(null);
-  const [topText, setTopText] = useState("");
+  const [texts, setTexts] = useState([]);
 
   const onFileChange = (e) => {
     setFile(e.target.files[0]);
@@ -16,7 +16,22 @@ const MemeUploadBar = ({ onUpload }) => {
     }
     const formData = new FormData();
     formData.append("image", file);
-    formData.append("topText", topText);
+    const tempText = {};
+    tempText.content = texts;
+    tempText.size = 32;
+    tempText.color = "WHITE";
+    tempText.alignmentX = "HORIZONTAL_ALIGN_CENTER";
+    tempText.alignmentY = "VERTICAL_ALIGN_TOP";
+
+    const tempText2 = {};
+    tempText2.content = texts;
+    tempText2.size = 32;
+    tempText2.color = "WHITE";
+    tempText2.alignmentX = "HORIZONTAL_ALIGN_CENTER";
+    tempText2.alignmentY = "VERTICAL_ALIGN_BOTTOM";
+
+    formData.append("texts", JSON.stringify(tempText));
+    formData.append("texts", JSON.stringify(tempText2));
     const response = await fetch(`${BACKEND_API}/api/memes`, {
       method: "POST",
       body: formData,
@@ -31,8 +46,8 @@ const MemeUploadBar = ({ onUpload }) => {
         <input type="file" onChange={onFileChange} />
         <input
           type="text"
-          onChange={(e) => setTopText(e.target.value)}
-          value={topText}
+          onChange={(e) => setTexts(e.target.value)}
+          value={texts}
         />
         <input type="submit" value="Upload Meme" />
       </form>
