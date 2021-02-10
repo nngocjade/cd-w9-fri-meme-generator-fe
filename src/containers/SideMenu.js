f9d16de51db5a29bd6351619e36fbfe593d02adf
nngocjade <nngocjade@gmail.com>
2021-02-10 18:29:05 +0700

    added pages and routes

---
+++ src/containers/SideMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Nav } from "react-bootstrap";
+
+const SideMenu = () => {
+  return (
+    <Nav className="col-md-3 d-md-block bg-light sidebar collapse">
+      <div className="sidebar-sticky pt-3"></div>
+    </Nav>
+  );
+};
+
+export default SideMenu;
