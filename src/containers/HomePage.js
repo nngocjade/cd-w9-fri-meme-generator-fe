554a2000f9d4e66abd3d310fde82292fde64f065
nngocjade <nngocjade@gmail.com>
2021-02-10 18:33:42 +0700

    moved fetch req and res from App.js to HomePage.js

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import MemeUploadBar from "../components/MemeUploadBar";
+import MemeList from "../components/MemeList";
+
+const BACKEND_API = process.env.REACT_APP_BACKEND_API;
+console.log("BACKEND_API", BACKEND_API);
 
 const HomePage = () => {
+  const [memes, setMemes] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+
+  const handleUpload = () => {
+    setRefresh(true);
+  }; //this handles auto refresh when a new meme is added
+
+  useEffect(() => {
+    async function fetchData() {
+      const url = `${BACKEND_API}/api/memes`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log("data", data);
+      console.log("data.data", data.data);
+      if (data && data.data) {
+        setMemes(data.data);
+      }
+      setRefresh(false);
+    }
+    fetchData();
+  }, [refresh]);
   return (
     <div>
-      <h1>Home Page</h1>
+      <MemeUploadBar onUpload={handleUpload} />
+      <MemeList memes={memes} />
     </div>
   );
 };
